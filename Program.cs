5b5c69d7b4609cc18945ef34718fbc1b89f55d64
NarcisaArreagaS <nl.arreagas@uea.edu.ec>
2025-06-07 16:32:28 -0400

    Update Program.cs
    program

--- Program.cs
+++ Program.cs
@@ -18,45 +18,5 @@ class Program
         Console.WriteLine("Perímetro: " + miRectangulo.CalcularPerimetro());
     }
 }
-// Clase Circulo representa un círculo con un radio
-public class Circulo
-{
-    private double radio;
-    // Constructor que recibe el radio
-    public Circulo(double radio)
-    {
-        this.radio = radio;
-    }
-    // Calcula el área del círculo
-    public double CalcularArea()
-    {
-        return Math.PI * radio * radio;
-    }
-    // Calcula el perímetro del círculo
-    public double CalcularPerimetro()
-    {
-        return 2 * Math.PI * radio;
-    }
-}
-// Clase Rectangulo representa un rectángulo con base y altura
-public class Rectangulo
-{
-    private double baseRectangulo;
-    private double altura;
-    // Constructor que recibe la base y altura
-    public Rectangulo(double baseRectangulo, double altura)
-    {
-        this.baseRectangulo = baseRectangulo;
-        this.altura = altura;
-    }
-    // Calcula el área del rectángulo
-    public double CalcularArea()
-    {
-        return baseRectangulo * altura;
-    }
-    // Calcula el perímetro del rectángulo
-    public double CalcularPerimetro()
-    {
-        return 2 * (baseRectangulo + altura);
-    }
-} 
+
+
